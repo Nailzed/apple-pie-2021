872333d7202f322f8c08e2205ea5551c04a4c05f
Сергей Земсков <nailzed@yandex.ru>
2021-06-08 10:28:59 +0300

    Initial Commit

---
+++ apple pie 2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apple pie 2021
+//
+//  Created by Сергей Земсков on 08.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
